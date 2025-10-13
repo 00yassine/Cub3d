97001743e235fddbfaee0f5f1e55fa2bf70c6995
Yassine Kabili <ykabili-@e1r7p1.1337.ma>
2025-10-13 16:34:56 +0100

    finishing the minimap

--- src/raycasting/src/2_draw_2D_map.c
+++ src/raycasting/src/2_draw_2D_map.c
@@ -1,12 +1,12 @@
 #include "../../../includes/cub3d.h"
 
-#define MAX_MINIMAP_WIDTH 200
-#define MAX_MINIMAP_HEIGHT 200
-#define PLAYER_SIZE 4
-#define MINIMAP_OFFSET_X 10
-#define MINIMAP_OFFSET_Y 10
+#define MINIMAP_SIZE        200
+#define PLAYER_SIZE         4
+#define MINIMAP_OFFSET_X    10
+#define MINIMAP_OFFSET_Y    10
+#define TILE_SIZE           64
+#define SCALE_FACTOR        8.0  /* divide world coords by this to scale down */
 
-/* Draw a single pixel in the image */
 void	my_mlx_pixel_put_minimap(t_data *d, int x, int y, int color)
 {
 	char	*dst;
@@ -17,21 +17,16 @@ void	my_mlx_pixel_put_minimap(t_data *d, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-/* Draw one square cell of the minimap */
-static void	draw_square(t_data *d, int x, int y, int color, int cell_size)
+static void	draw_square(t_data *d, int sx, int sy, int color, int size)
 {
 	int	i;
 	int	j;
-	int	sx;
-	int	sy;
 
-	sy = y * cell_size;
 	i = 0;
-	while (i < cell_size)
+	while (i < size)
 	{
-		sx = x * cell_size;
 		j = 0;
-		while (j < cell_size)
+		while (j < size)
 		{
 			my_mlx_pixel_put_minimap(d, sx + j, sy + i, color);
 			j++;
@@ -40,65 +35,74 @@ static void	draw_square(t_data *d, int x, int y, int color, int cell_size)
 	}
 }
 
-/* Draw the player on the minimap */
-static void	draw_player(t_data *d, int cell_size)
+static void	draw_player(t_data *d)
 {
-	int		i;
-	int		j;
-	double	map_x;
-	double	map_y;
-	int		px;
-	int		py;
+	int	i;
+	int	j;
+	int	cx;
+	int	cy;
 
-	map_x = d->player.fpx / 64.0;
-	map_y = d->player.fpy / 64.0;
-	px = (int)(map_x * cell_size - (PLAYER_SIZE / 2));
-	py = (int)(map_y * cell_size - (PLAYER_SIZE / 2));
-
+	cx = MINIMAP_OFFSET_X + MINIMAP_SIZE / 2 - PLAYER_SIZE / 2;
+	cy = MINIMAP_OFFSET_Y + MINIMAP_SIZE / 2 - PLAYER_SIZE / 2;
 	i = 0;
 	while (i < PLAYER_SIZE)
 	{
 		j = 0;
 		while (j < PLAYER_SIZE)
 		{
-			my_mlx_pixel_put_minimap(d, px + j, py + i, 0xFF0000);
+			my_mlx_pixel_put_minimap(d, cx + j, cy + i, 0xFF0000);
 			j++;
 		}
 		i++;
 	}
 }
 
+/* Scaled minimap centered on player */
 void	draw_minimap(t_data *d)
 {
-	int	x;
-	int	y;
-	int	color;
-	int	cell_size;
-	int	cell_x;
-	int	cell_y;
+	int		x;
+	int		y;
+	int		color;
+	double	player_x;
+	double	player_y;
+	double	map_x;
+	double	map_y;
+	double	tile_screen_x;
+	double	tile_screen_y;
+	double	cell_size;
 
-	cell_x = MAX_MINIMAP_WIDTH / d->cols;
-	cell_y = MAX_MINIMAP_HEIGHT / d->rows;
-	if (cell_x < cell_y)
-		cell_size = cell_x;
-	else
-		cell_size = cell_y;
-	if (cell_size < 1)
-		cell_size = 1;
+	cell_size = TILE_SIZE / SCALE_FACTOR;
+	player_x = d->player.fpx;
+	player_y = d->player.fpy;
 	y = 0;
 	while (y < d->rows)
 	{
 		x = 0;
 		while (x < d->cols)
 		{
+			map_x = x * TILE_SIZE;
+			map_y = y * TILE_SIZE;
+			tile_screen_x = (map_x - player_x) / SCALE_FACTOR
+				+ MINIMAP_OFFSET_X + MINIMAP_SIZE / 2;
+			tile_screen_y = (map_y - player_y) / SCALE_FACTOR
+				+ MINIMAP_OFFSET_Y + MINIMAP_SIZE / 2;
+			if (tile_screen_x + cell_size < MINIMAP_OFFSET_X
+				|| tile_screen_y + cell_size < MINIMAP_OFFSET_Y
+				|| tile_screen_x > MINIMAP_OFFSET_X + MINIMAP_SIZE
+				|| tile_screen_y > MINIMAP_OFFSET_Y + MINIMAP_SIZE)
+			{
+				x++;
+				continue ;
+			}
 			if (d->map[y][x] == '1')
 				color = 0x333333;
 			else
 				color = 0xAAAAAA;
-			draw_square(d, x, y, color, cell_size);
+			draw_square(d, (int)tile_screen_x, (int)tile_screen_y, color,
+				(int)cell_size);
 			x++;
 		}
 		y++;
 	}
-	draw_player(d, cell_size);
+	draw_player(d);
 }
